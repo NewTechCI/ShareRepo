4d13e383a2b7b00dc321ec8249f5258dd800f75a
Masahiro Ohta <mume3mume3@gmail.com>
2016-03-24 10:58:13 +0900

    バックエンドをDockerに変更

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -1,3 +1,9 @@
+require 'docker'
 require 'serverspec'
 
-set :backend, :exec
+
+set :backend, :docker
+set :docker_url, ENV["DOCKER_HOST"]
+set :docker_image, "newtechci/docker-circleci"
+
+Excon.defaults[:ssl_verify_peer] = false
