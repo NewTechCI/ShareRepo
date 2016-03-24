6d24dde2574964f4a2a525604542f40ee4381c0b
Masahiro Ohta <mume3mume3@gmail.com>
2016-03-24 13:48:12 +0900

    CircleCI対応（docker rmを使わないようにする）

--- serverspec/spec/docker_nginx_spec.rb
+++ serverspec/spec/docker_nginx_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
+#CircleCI‘Î‰ž
+if ENV['CIRCLECI']
+ class Docker::Container
+  def remove(options={}); end
+  alias_method :delete, :remove
+ end
+end
+
 describe package('nginx') do
   it { should be_installed }
 end
