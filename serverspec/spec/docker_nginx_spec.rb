5d114ec84a814aefb39b532b937539d660ea5e20
Nobuyoshi Nagashima <n_nagasi1@gmail.com>
2016-03-29 00:31:33 +0900

    commt 8080:80

--- serverspec/spec/docker_nginx_spec.rb
+++ serverspec/spec/docker_nginx_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 #CircleCI�Ή�
-#if ENV['CIRCLECI']
-# class Docker::Container
-#  def remove(options={}); end
-#  alias_method :delete, :remove
-# end
-#end
+if ENV['CIRCLECI']
+ class Docker::Container
+  def remove(options={}); end
+  alias_method :delete, :remove
+ end
+end
 
 describe package('nginx') do
   it { should be_installed }
