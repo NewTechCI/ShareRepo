531ff14c09c197c8f35a5c11f1fb0b221d68509a
Nobuyoshi Nagashima <n_nagasi1@gmail.com>
2016-03-29 00:26:49 +0900

    commit 80:8080

--- exec-serverspec.sh
+++ exec-serverspec.sh
@@ -2,10 +2,10 @@
 
 cd serverspec
 #�e�X�g���ʂ��Ȃ��e�X�g�R�[�h
-bundle exec rspec spec/localhost/nginx_spec.rb
+#bundle exec rspec spec/localhost/nginx_spec.rb
 
 #�e�X�g���ʂ��e�X�g�R�[�h
 #bundle exec rspec spec/localhost/ruby_spec.rb
 
 #Docker�R���e�i�p
-#bundle exec rspec spec/docker_nginx_spec.rb
+bundle exec rspec spec/docker_nginx_spec.rb
