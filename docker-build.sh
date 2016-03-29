1eea4339ca13cca2e936eacfe66bb1ff8715a175
Nobuyoshi Nagashima <n_nagasi1@gmail.com>
2016-03-29 18:49:00 +0900

    commit

--- docker-build.sh
+++ docker-build.sh
@@ -4,17 +4,17 @@ set -xe
 if [ -e ~/cache/docker-circleci.tar ] && [ $(md5sum Dockerfile | cut -d' ' -f1) = $(cat ~/cache/dockerfile.digest) ]
 then
   docker load < ~/cache/docker-circleci.tar
-  docker push nnagasi/nnagasi/docker-circleci
+  #docker push nnagasi/nnagasi/docker-circleci
 
 else
   mkdir -p ~/cache
-  #docker build -t newtechci/docker-circleci .
-  docker build -t nnagasi/docker-circleci .
+  docker build -t newtechci/docker-circleci .
+  #docker build -t nnagasi/docker-circleci .
   md5sum Dockerfile | cut -d' ' -f1 > ~/cache/dockerfile.digest
-  #docker save newtechci/docker-circleci > ~/cache/docker-circleci.tar
-  docker save nnagasi/docker-circleci > ~/cache/docker-circleci.tar
+  docker save newtechci/docker-circleci > ~/cache/docker-circleci.tar
+  #docker save nnagasi/docker-circleci > ~/cache/docker-circleci.tar
 
-  docker push nnagasi/nnagasi/docker-circleci
+  #docker push nnagasi/nnagasi/docker-circleci
 
 #
 
