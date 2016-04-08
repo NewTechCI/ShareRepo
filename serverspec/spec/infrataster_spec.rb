d802709d6e994fa12cddb78f71e776d15838e133
Masahiro Ohta <mume3mume3@gmail.com>
2016-04-08 20:58:58 +0900

    url指定変更

--- serverspec/spec/infrataster_spec.rb
+++ serverspec/spec/infrataster_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe server(:ex) do
-    before { @url = "http://172.17.0.1/"}
-
-    describe http(@url) do
+    describe http('http://172.17.0.1/') do
         it 'returns 200' do
             expect(response.status).to eq(200)
         end
--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -17,5 +17,5 @@ Excon.defaults[:ssl_verify_peer] = false
 RSpec.configuration.include RSpec::JsonMatcher
 Infrataster::Server.define(
     :ex,
-    ENV["DOCKER_IP"]
+    '172.17.0.1'
 )
