dd75148c11eb6ef4dc89ae5e06de95a1a53ce7f5
Masahiro Ohta <mume3mume3@gmail.com>
2016-04-08 18:45:37 +0900

    修正

--- exec-serverspec.sh
+++ exec-serverspec.sh
@@ -4,7 +4,7 @@
 #docker inspect --format '{{.NetworkSettings.IPAddress}}' newtech
 
 #DOCKER_IP=`docker inspect --format '{{.NetworkSettings.IPAddress}}' newtech`
-echo $DOCKER_IP
+echo ENV["DOCKER_IP"]
 cd serverspec
 #テストが通らないテストコード
 #bundle exec rspec spec/localhost/nginx_spec.rb
