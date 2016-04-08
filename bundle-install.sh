b90cf3f8d151f0717dea207202c06616c479437b
Masahiro Ohta <mume3mume3@gmail.com>
2016-04-08 20:29:31 +0900

    デバッグ

--- bundle-install.sh
+++ bundle-install.sh
@@ -3,10 +3,10 @@
 cd serverspec
 ruby -v
 #infrataster“±“ü—p
-sudo yum install -y libxml2 libxml2-devel libxslt libxslt-devel
+yum install -y libxml2 libxml2-devel libxslt libxslt-devel
 bundle config build.nokogiri --use-system-libraties
 
-#rvm list known
+rvm list known
 #gem list net-ssh -ar
 #bundle update
 #cat Gemfile.lock
