3fc435e57bda288e10d62ada0038a6dd0496e872
Masahiro Ohta <mume3mume3@gmail.com>
2016-04-08 21:27:33 +0900

    nokogiriインストール修正

--- bundle-install.sh
+++ bundle-install.sh
@@ -3,7 +3,8 @@
 cd serverspec
 ruby -v
 #infrataster“±“ü—p
-apt-get install -y libxml2 libxml2-devel libxslt libxslt-develbundle config build.nokogiri --use-system-libraties
+apt-get install -y libxml2 libxml2-devel libxslt libxslt-devel
+bundle config build.nokogiri --use-system-libraties
 
 #for debug
 #rvm list known
